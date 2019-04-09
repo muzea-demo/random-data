e632c3dabe35f83d6f5376f966c76c31eff64b6a
muzea <mr.muzea@gmail.com>
2019-04-09 13:40:35 +0800

    test: 补充一个简单的unlink测试

--- test.mjs
+++ test.mjs
@@ -20,3 +20,27 @@ import { stringify } from './stringify.mjs'
   process.exit(1);
 })();
 
+
+(function unlink() {
+  const template = fs.readFileSync('./sample/graph.unlink.txt', { encoding: 'utf8' });
+  const result = parse(template);
+  const listResult = generate(...result);
+  const [limit, graph] = listResult;
+  const maxNodeNum = limit.values[0].value;
+  const edgeNum = limit.values[1].value;
+  const _c1 = limit.values[2].value;
+  const _c2 = limit.values[3].value;
+  const [c1, c2] = [_c1, _c2].sort((a, b) => a - b);
+  const edgeList = graph.values[0].value;
+  if (edgeList.length !== edgeNum) {
+    process.exit(1);
+  }
+  if (edgeList.some((edge) => {
+    if (c1 === edge[0] && c2 === edge[1]) {
+      return true;
+    }
+    return edge[0] <=0 || edge[1] <=0 || edge[0] > maxNodeNum || edge[1] > maxNodeNum;
+  })) {
+    process.exit(1);
+  }
+})();
